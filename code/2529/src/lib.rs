7008b0175f729d6609fd7e4f6cc04be5d92dfa7b
Eveeifyeve <eveeg1971@gmail.com>
2024-02-22 02:41:56 +1100

    chore: moved solutions to it's own code folder

--- code/2529/src/lib.rs
+++ code/2529/src/lib.rs
@@ -1,6 +1,8 @@
 struct Solution {}
 
-// Solution done by Github: Eveeifyeve 
+
+///! Solution 
+
 impl Solution {
     pub fn maximum_count(nums: Vec<i32>) -> i32 {
         let (num_neg, num_pos) = nums.iter().fold((0,0), |(num_neg, num_pos), &x | {(num_neg + (x < 0) as i32, num_pos + (x > 0) as i32)});
